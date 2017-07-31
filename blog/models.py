6b768f771642b3a2067e1f530613e99148c23824
mario <mario@marioui-MacBook-Pro.local>
2017-07-31 14:36:01 +0900

    장고 어드민

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,18 @@
+import re
+from django.forms import ValidationError
 from django.db import models
+
+def lnglat_validator(value):
+    if not re.match(r'^(\d+\.?\d*),(\d+\.?\d*)$', value):
+        raise ValidationError('Invalid LngLat Type')
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    tags = models.CharField(max_length=100, blank=True)
+    lnglat = models.CharField(max_length=50,
+        validators=[lnglat_validator],
+        help_text='경도,위도 포맷으로 입력', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
